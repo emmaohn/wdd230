49cf7da5ad3ad87fa7c27757e36adcb1202cc73a
emmaohn <joh20123@byui.edu>
2022-10-23 00:11:20 -0600

    responsive content

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -4,7 +4,20 @@ toggleNav = () => {
 
 document.getElementById('ham').addEventListener('click', toggleNav);
 
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 const today = new Date();
 document.getElementById('year').textContent = today.getFullYear();
+document.getElementById('todays-date').textContent = `${days[today.getDay()]}, ${today.getDate()} ${months[today.getMonth()]} ${today.getFullYear()}`;
+
 const lastmodified = new Date(document.lastModified);
 document.getElementById('lastModified').textContent = document.lastModified;
+
+let day = 1;
+// today.getDay()
+if (day == 1 || today.getDay() == 2) {
+    document.getElementById('meeting-banner').style.display = 'block';
+} else {
+    document.getElementById('meeting-banner').style.display = 'none';
+};
