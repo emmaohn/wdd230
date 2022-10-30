a7782e31ca1ea75a7c881ada191cc868f86a0149
emmaohn <joh20123@byui.edu>
2022-10-29 21:34:50 -0600

    discover page

--- chamber/js/discover.js
+++ chamber/js/discover.js
@@ -1,1 +1,7 @@
-// const visit =
+let lastvisited = localStorage.getItem('lastVisited') ?? new Date(),
+    diff = math.floor(new Date() - new Date(lastvisited) / 1000 / 60 / 60 / 24);
+
+// console.log(diff);
+localStorage.setItem('lastVisited', new Date());
+
+document.getElementById('lastvisit').textContent = 'You last visited this site' + diff + 'days ago';
--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,6 +1,7 @@
 
 let temp = 55;
-let windchill = 10;
+let windspeed = 10;
 let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
 
-document.getElementById('windChill');
+document.getElementById('windspeed').textContent = windspeed + 'MPH';
+document.getElementById('chill').textContent = chill;
