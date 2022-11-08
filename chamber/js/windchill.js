eb932ba95f05ce1e4a85d480a7bd1c1ce7fa210b
emmaohn <joh20123@byui.edu>
2022-11-08 14:52:16 -0700

    fixes

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,7 +1,13 @@
 
 let temp = 55;
 let windspeed = 10;
-let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+let chill
+
+if (temp <= 50 && windspeed > 3) {
+    chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+} else {
+    chill = 'N/A';
+};
 
 document.getElementById('windspeed').textContent = windspeed + 'MPH';
 document.getElementById('chill').textContent = chill;
