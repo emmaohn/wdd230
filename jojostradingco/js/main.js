f626cc22d164bff444773c4d87ddece764e85365
emmaohn <joh20123@byui.edu>
2022-12-15 12:04:23 -0600

    updated nav

--- jojostradingco/js/main.js
+++ jojostradingco/js/main.js
@@ -1,18 +1,27 @@
 let ham = document.getElementById("hambutton");
+let x = document.getElementById("navUl");
+let y = document.getElementById("hambutton");
+let z = document.getElementById("flexNavUl");
 
 ham.addEventListener("click", () => {
-    let x = document.getElementById("navUl");
-    let y = document.getElementById("hambutton");
-    if (y.style.display === "none") {
-        x.style.display = "flex";
+    if (x.style.display === "block") {
+        x.style.display = "none";
     } else {
-        if (x.style.display === "block") {
-            x.style.display = "none";
-        } else {
-            x.style.display = "block";
-        } 
+        x.style.display = "block";
     }
 });
+
+// function myFunction(a) {
+//     if (a.matches) { // If media query matches
+//         document.body.style.backgroundColor = "yellow";
+//     } else {
+//         document.body.style.backgroundColor = "pink";
+//     }
+// }
+
+// var a = window.matchMedia("(max-width: 700px)")
+// myFunction(x) // Call listener function at run time
+// x.addListener(myFunction) // Attach listener function on state changes
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
--- jojostradingco/js/weather.js
+++ jojostradingco/js/weather.js
@@ -1,24 +1,7 @@
 
-// let temp = 55;
-// let windspeed = 10;
-// let chill
-
-function validate(temp, windspeed) {
-    if (temp <= 50 && windspeed > 3) {
-        chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
-    } else {
-        chill = 'N/A';
-    };
-    return chill
-};
-
-// document.getElementById('windspeed').textContent = windspeed + 'MPH';
-// document.getElementById('chill').textContent = chill;
-
-
 // weatherurl = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
-const LAT = '40.0832';
-const LON = '74.0682';
+const LAT = '32.7555';
+const LON = '97.3308';
 const APIKEY = 'fc4826416b6e323a65f8dbe90ff92aea';
 // const iconid = `http://openweathermap.org/img/wn/${iconid}@2x.png`;
 const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${LAT}&lon=${LON}&appid=${APIKEY}`;
@@ -28,16 +11,12 @@ function showWeather(obj) {
     // let iconpath = document.querySelector('#icon-src');
     let weathericon = document.querySelector('#weathericon');
     let figurecaption = document.querySelector('figcaption');
-    let windspeed = document.querySelector('#windspeed');
-    // let chill = document.querySelector('#chill');
     const iconURL = `http://openweathermap.org/img/wn/${obj.weather[0].icon}@2x.png`;
     currenttemp.textContent = Math.round((obj.main.temp - 273.15) * 1.8000 + 32);
     // iconpath.textContent = iconURL;
     weathericon.setAttribute('src', iconURL);
     weathericon.setAttribute('alt', obj.weather[0].icon);
     figurecaption.textContent = obj.weather[0].description.toUpperCase();
-    windspeed.textContent = obj.wind.speed;
-    chill.textContent = `${validate(currenttemp, windspeed)}`;
 };
 
 fetch(apiURL)
